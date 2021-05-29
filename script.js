78fbf1233154da20fcbb9dffd64df34c842974e3
Methila Afrin Meem <50162707+Methila-Meem@users.noreply.github.com>
2021-05-30 01:46:43 +0600

    updated script

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+//function to compute interest 
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //declare variables to set values
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    //add validation for principal input and alert if not positive.
+    if(principal<=0){
+        alert("Enter a positive number.");
+        principal.focus;
+        return false;
+    }
+    else{
+        //calculate interest
+        var interest = principal*years*rate/100;
+        //convert No. of years into actual year
+        var year = new Date().getFullYear()+parseInt(years);
+        //Show result 
+        document.getElementById("result").innerHTML="\<br\>If you deposit \<mark\>"+principal+"\<\/mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<\/mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<\/mark\>,\<br\>in the year \<mark\>"+year+"\<\/mark\>.\<br\>";
+    }
 }
-        
+//function to read and display the value of the range slider 
+function updateRate(){
+        var rateval = document.getElementById("rate").value;
+        document.getElementById("rate_val").innerText=rateval;
+    }       
