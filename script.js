27255abf9f9041f5ad91f4d629cbf170dc073d10
Methila Afrin Meem <50162707+Methila-Meem@users.noreply.github.com>
2021-05-30 11:21:30 +0600

    fixed feature

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 function compute()
 {
     //declare variables to set values
-    var principal = document.getElementById("principal").value;
+    var principal = document.getElementById("principal");
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     
     //add validation for principal input and alert if not positive.
-    if(principal<=0){
+    if(principal.value<=0){
         alert("Enter a positive number.");
         principal.focus();
         return false;
     }
     else{
         //calculate interest
-        var interest = principal*years*rate/100;
+        var interest = principal.value*years*rate/100;
         //convert No. of years into actual year
         var year = new Date().getFullYear()+parseInt(years);
         //Show result 
-        document.getElementById("result").innerHTML="\<br\>If you deposit \<mark\>"+principal+"\<\/mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<\/mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<\/mark\>,\<br\>in the year \<mark\>"+year+"\<\/mark\>.\<br\>";
+        document.getElementById("result").innerHTML="\<br\>If you deposit \<mark\>"+principal.value+"\<\/mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<\/mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<\/mark\>,\<br\>in the year \<mark\>"+year+"\<\/mark\>.\<br\>";
     }
 }
 //function to read and display the value of the range slider 
